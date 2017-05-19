547836710cf73280f3859b2e4894d166ef31f0a2
Carl Woodhouse <carl.woodhouse@gmail.com>
2017-05-19 08:27:44 +0100

    Instagram views, indented home

---
+++ ResourceManifest.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.UI.Resources;
+
+namespace Orchard.Cw.Bootstrap4 {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineStyle("Bootstrap.Cw.Style").SetUrl("bs.min.css", "bs.css");
+        }
+    }
+}
